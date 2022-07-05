60d695527471ea26191b257b903b92a6aa7c492c
jmigsan <>
2022-07-06 00:16:29 +0100

    get chakra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 function App() {
   return (
-    <div>
-      oi
-    </div>
+    <ChakraProvider>
+      <div>
+        oi
+      </div>
+    </ChakraProvider>
   );
 }
 
