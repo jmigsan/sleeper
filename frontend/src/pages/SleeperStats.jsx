d7542ac84db3fe20eec442d41d196c8ab10fd7d2
jmigsan <>
2022-07-06 23:49:24 +0100

    sl stats

--- frontend/src/pages/SleeperStats.jsx
+++ frontend/src/pages/SleeperStats.jsx
@@ -1,6 +1,19 @@
+import {
+  Container,
+  Heading,
+} from '@chakra-ui/react'
+
 const SleeperStats = () => {
   return (
-    <div>SleeperStats</div>
+    <Container maxW={'5xl'} p={4}>
+      <Heading as='h1' size='2xl'>
+        Sleeper Stats
+      </Heading>
+
+      
+
+    </Container>
   )
-}
+};
+
 export default SleeperStats
