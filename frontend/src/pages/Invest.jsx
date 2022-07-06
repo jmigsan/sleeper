932751d3efcae1afc204ba6f7cb1dbc3b26b1f87
jmigsan <>
2022-07-06 23:10:05 +0100

    invest

---
+++ frontend/src/pages/Invest.jsx
@@ -0,0 +1,19 @@
+import {
+  Container,
+  Heading,
+} from '@chakra-ui/react'
+
+const Portfolio = () => {
+  return (
+    <Container maxW={'5xl'} p={4}>
+      <Heading as='h1' size='2xl'>
+        Invest
+      </Heading>
+
+      
+
+    </Container>
+  )
+};
+
+export default Portfolio
