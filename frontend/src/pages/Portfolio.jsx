f8b4880c9fc879267e4eb13a550b68b8860ed2fd
jmigsan <>
2022-07-06 23:09:20 +0100

    pioioiolo

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,14 +1,19 @@
 import {
+  Container,
   Heading,
 } from '@chakra-ui/react'
 
 const Portfolio = () => {
   return (
     <Container maxW={'5xl'} p={4}>
-      <Heading>
+      <Heading as='h1' size='2xl'>
         Portfolio
       </Heading>
+
+      
+
     </Container>
   )
-}
+};
+
 export default Portfolio
