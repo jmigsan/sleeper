689f0b0fbb64726c39261ac10bca865993d75f75
jmigsan <>
2022-07-13 17:34:42 +0100

    invest buttons

--- frontend/src/components/SleeperStats/SStatsInvest.jsx
+++ frontend/src/components/SleeperStats/SStatsInvest.jsx
@@ -13,24 +13,67 @@ import {
   HStack,
   Center,
   Stack,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  ButtonGroup,
+  Tabs, 
+  TabList, 
+  TabPanels, 
+  Tab, 
+  TabPanel,
+  Box,
+  Divider,
 } from '@chakra-ui/react'
 
 import { AddIcon, MinusIcon } from '@chakra-ui/icons'
+import { useState } from "react";
 
 const SStatsInvest = () => {
+  const [buySellToggle, setBuySellToggle] = useState('buy');
+
   return (
     <Center pb={4}>
       <Stack>
         <Center>
           <Heading size={'lg'}>Invest</Heading>
         </Center>
+        <HStack height={95}>
+          <Stack>
+            <NumberInput size='lg' maxW={40} defaultValue={0}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <Button bg={'green.200'} rightIcon={<AddIcon />}>
+              Buy
+            </Button>
+          </Stack>
+          <Divider orientation='vertical' />
+          <Stack>
+            <NumberInput size='lg' maxW={40} defaultValue={0}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <Button bg={'red.200'} rightIcon={<MinusIcon />}>
+              Sell
+            </Button>
+          </Stack>
+
+        </HStack>
+
+        
+        
         <HStack>
-          <Button bg={'green.200'} rightIcon={<AddIcon />}>
-            Buy
-          </Button>
-          <Button bg={'red.200'} rightIcon={<MinusIcon />}>
-            Sell
-          </Button>
+          
+          
         </HStack>
       </Stack>
     </Center>
